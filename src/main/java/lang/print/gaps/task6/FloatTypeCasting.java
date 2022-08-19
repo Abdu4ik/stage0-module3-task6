6013adde00d5c433a88e4ec9c586571699e6a239
Abdu4ik <107094143+Abdu4ik@users.noreply.github.com>
2022-08-20 02:55:06 +0500

    Update FloatTypeCasting.java

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,18 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+
+        float lastBit = numberToBeRounded - ((int) numberToBeRounded);
+        
+        if (lastBit < 0.5){
+
+            System.out.println((int) numberToBeRounded);
+            
+        }else{
+
+            System.out.println(((int) numberToBeRounded) + 1);
+            
+        }
+
     }
 }
